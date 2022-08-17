9cfabd74b2010db4e904af8406a11d647d53e09e
StormIrbis <pharad2013@gmail.com>
2022-08-17 23:30:45 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?');
+answers[1] = prompt('Какая ваша фамилия?');
+answers[2] = prompt('Сколько вам лет?');
+
+document.write(answers);
